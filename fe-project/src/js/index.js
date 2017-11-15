9c4d1df589e05fe16cb5ec4ca1c7890e1311cb7e
Qiutc <TongchengQiu@gmail.com>
2017-11-16 00:47:52 +0800

    update

--- fe-project/src/js/index.js
+++ fe-project/src/js/index.js
@@ -47,7 +47,29 @@ $(function () {
           <img src="' + ret.headimg + '" alt="AVATAR">\
           <h6>'+ ret.username +'</h6>\
         </a>\
+        <a href="/userdata/logout" class="btn-layout">退出</a>\
       ');
+
+      if (ret.identity === 0 || ret.identity === 2) {
+        $('.video-hide').show();
+      }
     }
   })
 });
+
+
+
+
+  function getUrlParam(name) {
+      var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+      var r = window.location.search.substr(1).match(reg);
+      if (r != null) return (r[2]); return '';
+  }
+
+  function param(obj) {
+    var arr = [];
+    for (var key in obj) {
+      arr.push(key + '=' + obj[key]);
+    }
+    return arr.join('&');
+  }
