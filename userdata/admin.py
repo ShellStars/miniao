dcad10943514c2415a6fb8c3c81560ed924f48a5
orangesea <378936393@qq.com>
2017-12-16 00:19:01 +0800

    update
    update

--- userdata/admin.py
+++ userdata/admin.py
@@ -5,6 +5,6 @@ from .models import Userinfo
 
 class UserinfoAdmin(admin.ModelAdmin):
     list_display = ('username', 'sex', 'telnum', 'identity', 'integralnum', 'addtime', 'ispass', 'isfgpwd')
-
+    search_fields = ('username',)
 
 admin.site.register(Userinfo, UserinfoAdmin)
