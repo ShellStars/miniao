5906334fe413d6d70ce25a2aec3ba15ead2b6db3
orangesea <378936393@qq.com>
2017-12-20 21:11:57 +0800

    update
    update

--- minicms/views.py
+++ minicms/views.py
@@ -41,7 +41,7 @@ def index(request):
     cursor.execute("select * from other_lunbopic where published=True limit 5")
     lunboinfo = dictfetchall(cursor)
     cursor.execute(
-        "select * from meeting_meetarticle where column_id='yugao' and published=True order by id desc limit 5")
+        "select * from meeting_meetarticle where column_id='无无无' and published=True order by id desc limit 5")
     yugaoinfo = dictfetchall(cursor)
     cursor.execute("select * from column_infoarticle where published=True order by id desc limit 5")
     zixuninfo = dictfetchall(cursor)
--- userdata/views.py
+++ userdata/views.py
@@ -215,13 +215,13 @@ def register(request):
                 except:
                     checknum1 = ''
                 if len(telnum1) > 0:
-                    # dic = {'info': '用户已存在'}
-                    # return render_to_response('register.html', dic)
-                    return HttpResponse(json.dumps({'info': '用户已存在'}), content_type="application/json")
+                    dic = {'info': '用户已存在'}
+                    return render_to_response('register.html', dic)
+                    # return HttpResponse(json.dumps({'info': '用户已存在'}), content_type="application/json")
                 elif checknum1 == '' or str(checknum) != checknum1:
-                    # dic = {'info': '验证码错误'}
-                    # return render_to_response('register.html', dic)
-                    return HttpResponse(json.dumps({'info': '验证码错误'}), content_type="application/json")
+                    dic = {'info': '验证码错误'}
+                    return render_to_response('register.html', dic)
+                    # return HttpResponse(json.dumps({'info': '验证码错误'}), content_type="application/json")
                 else:
                     username = request.POST['username']
                     password = request.POST['password']
